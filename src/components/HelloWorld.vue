d837b3758bc9ff6bec164b51bfa537ce4c10139a
xiaotangmu <1275485428@qq.com>
2020-11-20 16:14:10 +0800

    xxx

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,10 +1,27 @@
 <template>
   <div class="home">
     <h1>前端演示SignalR</h1>
-    <input v-model="user" type="text" />
-    <input v-model="message" type="text" />
-    <button @click="sendAll">发送全部</button>
-    <button @click="sendOwn">对自己发送</button>
+    <div class="content">
+      <label class="tip">ToUser</label>
+      <input v-model="user" type="text" />
+      <label class="tip">ToUserId</label>
+      <input v-model="userId" type="text" />
+    </div>
+    <div class="content" style="margin: 10px auto">
+      <label class="tip">Message</label>
+      <input v-model="message" type="text" />
+      <button @click="sendAll">发送全部</button>
+      <button @click="sendOwn">发给自己</button>
+      <button @click="sendToGroup">发送组</button>
+      <button @click="sendOne">发给指定Id</button>
+    </div>
+    <div class="content">
+      <label class="tip">Group</label>
+      <input v-model="groupName" type="text" />
+      <button @click="joinGroup">加入组</button>
+      <button @click="levelGroup">离开组</button>
+    </div>
+
     <div>
       <ul v-for="(item ,index) in messages" v-bind:key="index +'itemMessage'">
         <li>{{item.user}} says {{item.message}}</li>
@@ -25,16 +42,26 @@
     data() {
       return {
         user: "", //用户
+        userId: "", //用户Id
         message: "", //消息
         connection: "", //signalr连接
-        messages: [] //返回消息
+        messages: [], //返回消息
+        groupName: "", // 组名
       };
     },
     methods: {
+      // 发送给指定User
+      sendOne: function() {
+        this.connection
+          .invoke("Echo", this.userId, this.message) // invoke 连接后台方法
+          .catch(function(err) {
+            return console.error(err);
+          });
+      },
       //给全部发送消息
       sendAll: function() {
         this.connection
-          .invoke("SendMessage", this.user, this.message)
+          .invoke("SendMessage", this.user, this.message) // invoke 连接后台方法
           .catch(function(err) {
             return console.error(err);
           });
@@ -46,20 +73,43 @@
           .catch(function(err) {
             return console.error(err);
           });
-      }
-
-
+      },
+      // 加入组
+      joinGroup: function() {
+        this.connection
+          .invoke("JoinGroup", this.groupName)
+          .catch(function(err) {
+            return console.error(err);
+          });
+      },
+      // 发送信息给所在组
+      sendToGroup: function() {
+        this.connection
+          .invoke("SendToGroup", this.groupName, this.message)
+          .catch(function(err) {
+            return console.error(err);
+          });
+      },
+      // 离开组
+      levelGroup: function() {
+        this.connection
+          .invoke("LeaveGroup", this.groupName)
+          .catch(function(err) {
+            return console.error(err);
+          });
+      },
     },
     created: function() {
       let thisVue = this;
       this.connection = new signalR.HubConnectionBuilder()
+        // .withUrl("https://localhost:8789/api/chatHub?username=xxx", { // 带参
         .withUrl("https://localhost:5001/chathub", {
           skipNegotiation: true,
-          transport: signalR.HttpTransportType.WebSockets
+          transport: signalR.HttpTransportType.WebSockets,
         })
         .configureLogging(signalR.LogLevel.Information)
         .build();
-      this.connection.on("ReceiveMessage", function(user, message) {
+      this.connection.on("ReceiveMessage", function(user, message) {  // ReceiveMessage 后台约定的接收方法
         thisVue.messages.push({ user, message });
         console.log({ user, message });
       });
@@ -72,3 +122,16 @@
     }
   };
 </script>
+<style>
+  .content{
+    width: 560px;
+    text-align: left;
+    /*background-color: #bfa;*/
+    margin: 0 auto;
+  }
+  .tip{
+    display: inline-block;
+    width: 80px;
+  }
+
+</style>
