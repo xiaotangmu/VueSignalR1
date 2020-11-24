b036834c88ca71099963cbfa8249a9b73b2551cb
xiaotangmu <1275485428@qq.com>
2020-11-24 17:08:57 +0800

    xx

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -21,11 +21,27 @@
       <button @click="joinGroup">加入组</button>
       <button @click="levelGroup">离开组</button>
     </div>
-
     <div>
       <ul v-for="(item ,index) in messages" v-bind:key="index +'itemMessage'">
         <li>{{item.user}} says {{item.message}}</li>
       </ul>
+    </div>
+
+    <hr>
+    <div>
+      <h3>登录后使用SignalR</h3>
+      <div class="content">
+        <label class="tip">UserName</label>
+        <input v-model="loginName" type="text" />
+      </div>
+      <div class="content" style="margin: 10px auto">
+        <label class="tip">Password</label>
+        <input v-model="password" type="text" @keyup.13="login"/>
+      </div>
+      <div class="content" >
+        <span style="display: inline-block; width: 80px"></span>
+        <button style="padding: 0 30px" @click="getTestManagement" >getTestManagement</button>
+      </div>
     </div>
   </div>
 </template>
@@ -34,6 +50,8 @@
   // @ is an alias to /src
   import HelloWorld from "@/components/HelloWorld.vue";
   import * as signalR from "@aspnet/signalr";
+  import axios from 'axios';
+
   export default {
     name: "Home",
     components: {
@@ -47,80 +65,108 @@
         connection: "", //signalr连接
         messages: [], //返回消息
         groupName: "", // 组名
+        loginName: '',
+        password: '',
       };
     },
     methods: {
+      getTestManagement: function(){
+        axios.get('https://localhost:8789/api/v0.1/Test/TestHub')
+          .then(function (response) {
+            console.log(response);
+          })
+          .catch(function (error) {
+            console.log(error);
+          });
+      },
+      // 登录
+      login: function() {
+        let that = this;
+        axios.post('https://localhost:8789/api/v0.1/LoginManagement/LogInWithAccount', {
+          LoginName: this.loginName,
+          Password: this.password,
+        })
+          .then(function (response) {
+            that.connect();
+            console.log(response);
+          })
+          .catch(function (error) {
+            console.log(error);
+          });
+      },
       // 发送给指定User
-      sendOne: function() {
+      sendOne() {
         this.connection
           .invoke("Echo", this.userId, this.message) // invoke 连接后台方法
-          .catch(function(err) {
+          .catch(function (err) {
             return console.error(err);
           });
       },
       //给全部发送消息
-      sendAll: function() {
+      sendAll() {
         this.connection
           .invoke("SendMessage", this.user, this.message) // invoke 连接后台方法
-          .catch(function(err) {
+          .catch(function (err) {
             return console.error(err);
           });
       },
       //只给自己发送消息
-      sendOwn: function() {
+      sendOwn() {
         this.connection
           .invoke("SendMessageCaller", this.message)
-          .catch(function(err) {
+          .catch(function (err) {
             return console.error(err);
           });
       },
       // 加入组
-      joinGroup: function() {
+      joinGroup() {
         this.connection
           .invoke("JoinGroup", this.groupName)
-          .catch(function(err) {
+          .catch(function (err) {
             return console.error(err);
           });
       },
       // 发送信息给所在组
-      sendToGroup: function() {
+      sendToGroup() {
         this.connection
           .invoke("SendToGroup", this.groupName, this.message)
-          .catch(function(err) {
+          .catch(function (err) {
             return console.error(err);
           });
       },
       // 离开组
-      levelGroup: function() {
+      levelGroup() {
         this.connection
           .invoke("LeaveGroup", this.groupName)
-          .catch(function(err) {
+          .catch(function (err) {
             return console.error(err);
           });
       },
-    },
-    created: function() {
-      let thisVue = this;
-      this.connection = new signalR.HubConnectionBuilder()
-        // .withUrl("https://localhost:8789/api/chatHub?username=xxx", { // 带参
-        .withUrl("https://localhost:5001/chathub", {
-          skipNegotiation: true,
-          transport: signalR.HttpTransportType.WebSockets,
-        })
-        .configureLogging(signalR.LogLevel.Information)
-        .build();
-      this.connection.on("ReceiveMessage", function(user, message) {  // ReceiveMessage 后台约定的接收方法
-        thisVue.messages.push({ user, message });
-        console.log({ user, message });
-      });
-      this.connection.on("ReceiveCaller", function(message) {
-        let user = "自己";//这里为了push不报错，我就弄了一个默认值。
-        thisVue.messages.push({ user, message });
-        console.log({ user, message });
-      });
-      this.connection.start();
+      // created: function() {  // 进来直接连接
+      connect: function() {
+        console.log('Hello 我已经开始连接了');
+        let thisVue = this;
+        this.connection = new signalR.HubConnectionBuilder()
+          // .withUrl("https://localhost:8789/api/chatHub?username=xxx", { // 带参, 注意路径不能在普通路径下（api/v0.1/）
+          .withUrl("https://localhost:8789/api/chatHub?LoginName=" + this.loginName, {
+            skipNegotiation: true,
+            transport: signalR.HttpTransportType.WebSockets,
+          })
+          .configureLogging(signalR.LogLevel.Information)
+          .build();
+        this.connection.on("ReceiveMessage", function (user, message) {  // ReceiveMessage 后台约定的接收方法
+          thisVue.messages.push({user, message});
+          console.log({user, message});
+        });
+        this.connection.on("ReceiveCaller", function (message) {
+          let user = "自己";//这里为了push不报错，我就弄了一个默认值。
+          thisVue.messages.push({user, message});
+          console.log({user, message});
+        });
+        this.connection.start();
+      },
     }
-  };
+  }
 </script>
 <style>
   .content{
